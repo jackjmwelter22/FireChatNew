e8663283e1405ecc6d1ffa584e9a0dc523963728
Jack Welter <jackjmwelter22@Jacks-MBP.hsd1.ga.comcast.net>
2018-04-21 12:24:32 -0400

    First Commit

---
+++ app/src/main/java/com/example/jackjmwelter22/firechat/MessageViewHolder.java
@@ -0,0 +1,16 @@
+package com.example.jackjmwelter22.firechat;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+public class MessageViewHolder extends RecyclerView.ViewHolder{
+    TextView msgTextView;
+
+            public MessageViewHolder(View v){
+                super(v);
+
+            }
+
+
+}
